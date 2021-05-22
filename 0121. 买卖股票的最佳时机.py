3fa796355092b104e1e8d93acdd3ba52ec306c7b
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-05-22 22:18:08 +0800

    Update 0121. 买卖股票的最佳时机.py

--- 0121. 买卖股票的最佳时机.py
+++ 0121. 买卖股票的最佳时机.py
@@ -10,3 +10,19 @@
 输出：5
 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+
+     
+遍历价格数组一遍，记录历史最低点，然后在每一天考虑这么一个问题：
+如果我是在历史最低点买进的，那么我今天卖出能赚多少钱？
+当考虑完所有天数之时，我们就得到了最好的答案。
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        inf = int(1e9)
+        minprice = inf
+        maxprofit = 0
+        for price in prices:
+            maxprofit = max(price - minprice, maxprofit)
+            minprice = min(price, minprice)
+        return maxprofit
+
