4dfd43b1d789dada544cb674e7ba394f6995f042
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-05-23 13:56:22 +0800

    Update 0123. 买卖股票的最佳时机 III.py

--- 0123. 买卖股票的最佳时机 III.py
+++ 0123. 买卖股票的最佳时机 III.py
@@ -25,3 +25,29 @@
 输出：0 
 解释：在这个情况下, 没有交易完成, 所以最大利润为 0。
 
+# dp 时间复杂度：O(n)
+由于我们最多可以完成两笔交易，因此在任意一天结束之后，我们会处于以下五个状态中的一种：
+
+未进行过任何操作；
+
+只进行过一次买操作；
+
+进行了一次买操作和一次卖操作，即完成了一笔交易；
+
+在完成了一笔交易的前提下，进行了第二次买操作；
+
+完成了全部两笔交易。
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        buy1 = buy2 = -prices[0]
+        sell1 = sell2 = 0
+        for i in range(1, n):
+            buy1 = max(buy1, -prices[i])
+            sell1 = max(sell1, buy1 + prices[i])
+            buy2 = max(buy2, sell1 - prices[i])
+            sell2 = max(sell2, buy2 + prices[i])
+        return sell2
+
+
