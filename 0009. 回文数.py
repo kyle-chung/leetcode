8adcf5d441e823f2d67b5e3ef475bd5166e32f82
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-05 20:08:08 +0800

    Create 0009. 回文数.py

---
+++ 0009. 回文数.py
@@ -0,0 +1,25 @@
+给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+
+回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
+
+示例 1：
+
+输入：x = 121
+输出：true
+
+示例 2：
+
+输入：x = -121
+输出：false
+解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+
+# 额外条件：不使用字符串
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        revertedNumber = 0
+        while x > revertedNumber:
+            revertedNumber = revertedNumber * 10 + x % 10
+            x //= 10
+        return x == revertedNumber or x == revertedNumber // 10
