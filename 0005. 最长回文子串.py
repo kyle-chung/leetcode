06215db2c36b5a4b63d74d511a3328bca3562571
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-05 14:10:07 +0800

    Update 0005. 最长回文子串.py

--- 0005. 最长回文子串.py
+++ 0005. 最长回文子串.py
@@ -11,6 +11,56 @@
 输入：s = "cbbd"
 输出："bb"
 
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/longest-palindromic-substring
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+# dp
+对于一个子串而言，如果它是回文串，并且长度大于 2，那么将它首尾的两个字母去除之后，它仍然是个回文串。
+
+那么我们就可以写出动态规划的状态转移方程：
+P(i,j) = P(i+1, j-1) ^ (Si == Sj)
+
+上文的所有讨论是建立在子串长度大于 2 的前提之上的，我们还需要考虑动态规划中的边界条件，即子串的长度为 1 或 2。
+我们可以写出动态规划的边界条件：
+P(i,i) = true
+P(i,i+1) = (Si == S(i+1))	
+
+最终的答案即为所有 P(i, j) = true 中 j-i+1（即子串长度）的最大值。
+注意：在状态转移方程中，我们是从长度较短的字符串向长度较长的字符串进行转移的，因此一定要注意动态规划的循环顺序。
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n < 2:
+            return s
+        
+        max_len = 1
+        begin = 0
+        # dp[i][j] 表示 s[i..j] 是否是回文串
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = True
+        
+        # 递推开始
+        # 先枚举子串长度
+        for L in range(2, n + 1):
+            # 枚举左边界，左边界的上限设置可以宽松一些
+            for i in range(n):
+                # 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+                j = L + i - 1
+                # 如果右边界越界，就可以退出当前循环
+                if j >= n:
+                    break
+                    
+                if s[i] != s[j]:
+                    dp[i][j] = False 
+                else:
+                    if j - i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
+                
+                # 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+                if dp[i][j] and j - i + 1 > max_len:
+                    max_len = j - i + 1
+                    begin = i
+        return s[begin:begin + max_len]
+
+
