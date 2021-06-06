ee0a2aac11be1931826ed0e08594ec99ea3cc461
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-06 14:31:24 +0800

    Update 0011. 盛最多水的容器.py

--- 0011. 盛最多水的容器.py
+++ 0011. 盛最多水的容器.py
@@ -12,3 +12,17 @@
 输入：height = [4,3,2,1,4]
 输出：16
 
+# 双指针 时间复杂度 O(N)
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i, j, res = 0, len(height) - 1, 0
+        while i < j:
+            if height[i] < height[j]:
+                res = max(res, height[i] * (j - i))
+                i += 1
+            else:
+                res = max(res, height[j] * (j - i))
+                j -= 1
+        return res
+
+
