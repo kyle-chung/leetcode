aa5fd030c08667a72c27a1c600bb476c87438400
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-07 14:35:10 +0800

    Create 0014. 最长公共前缀.py

---
+++ 0014. 最长公共前缀.py
@@ -0,0 +1,29 @@
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1：
+
+输入：strs = ["flower","flow","flight"]
+输出："fl"
+
+示例 2：
+
+输入：strs = ["dog","racecar","car"]
+输出：""
+解释：输入不存在公共前缀。
+
+# 纵向扫描 时间复杂度：O(mn)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        
+        length, count = len(strs[0]), len(strs)
+        for i in range(length):
+            c = strs[0][i]
+            if any(i == len(strs[j]) or strs[j][i] != c for j in range(1, count)):
+                return strs[0][:i]
+        
+        return strs[0]
+
