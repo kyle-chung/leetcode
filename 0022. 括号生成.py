f40edd88fab19561052547d28870775e67ce8bbc
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-11 15:43:47 +0800

    Update 0022. 括号生成.py

--- 0022. 括号生成.py
+++ 0022. 括号生成.py
@@ -10,4 +10,47 @@
 输入：n = 1
 输出：["()"]
 
+可以对所有情况进行遍历：
+
+"(" + [i=p时所有括号的排列组合] + ")" + [i=q时所有括号的排列组合]
+
+其中 p + q = n-1，且 p q 均为非负整数。
+
+事实上，当上述 p 从 0 取到 n-1，q 从 n-1 取到 0 后，所有情况就遍历完了。
+
+注：上述遍历是没有重复情况出现的，即当 (p1,q1)≠(p2,q2) 时，按上述方式取的括号组合一定不同。
+
+# dp
+对所有情况进行遍历：
+
+"(" + 【i=p时所有括号的排列组合】 + ")" + 【i=q时所有括号的排列组合】
+
+其中 p + q = n-1，且 p q 均为非负整数。
+
+事实上，当上述 p 从 0 取到 n-1，q 从 n-1 取到 0 后，所有情况就遍历完了。
+
+注：上述遍历是没有重复情况出现的，即当 (p1,q1)≠(p2,q2) 时，按上述方式取的括号组合一定不同。
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n == 0:
+            return []
+        total_l = []
+        total_l.append([None])    # 0组括号时记为None
+        total_l.append(["()"])    # 1组括号只有一种情况
+        for i in range(2,n+1):    # 开始计算i组括号时的括号组合
+            l = []        
+            for j in range(i):    # 开始遍历 p q ，其中p+q=i-1 , j 作为索引
+                now_list1 = total_l[j]    # p = j 时的括号组合情况
+                now_list2 = total_l[i-1-j]    # q = (i-1) - j 时的括号组合情况
+                for k1 in now_list1:  
+                    for k2 in now_list2:
+                        if k1 == None:
+                            k1 = ""
+                        if k2 == None:
+                            k2 = ""
+                        el = "(" + k1 + ")" + k2
+                        l.append(el)    # 把所有可能的情况添加到 l 中
+            total_l.append(l)    # l这个list就是i组括号的所有情况，添加到total_l中，继续求解i=i+1的情况
+        return total_l[n]
 
