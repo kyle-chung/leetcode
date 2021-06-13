5a6fb36391ad7e21d00649124b94e724967595da
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-13 14:05:31 +0800

    Update 0031. 下一个排列.py

--- 0031. 下一个排列.py
+++ 0031. 下一个排列.py
@@ -14,4 +14,26 @@
 输入：nums = [3,2,1]
 输出：[1,2,3]
 
+# 时间复杂度：O(N)
+我们需要将一个左边的「较小数」与一个右边的「较大数」交换，以能够让当前排列变大，从而得到下一个排列。
+
+同时我们要让这个「较小数」尽量靠右，而「较大数」尽可能小。当交换完成后，「较大数」右边的数需要按照升序重新排列。
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while j >= 0 and nums[i] >= nums[j]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        # 排序
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
 
