efeef9431c238189d7452b2bee7dd2cfeaf91534
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-14 23:16:57 +0800

    Update 0033. 搜索旋转排序数组.py

--- 0033. 搜索旋转排序数组.py
+++ 0033. 搜索旋转排序数组.py
@@ -15,3 +15,35 @@
 
 输入：nums = [4,5,6,7,0,1,2], target = 3
 输出：-1
+
+# 二分
+将数组一分为二，其中一定有一个是有序的，另一个可能是有序，也能是部分有序。
+此时有序部分用二分法查找。无序部分再一分为二，其中一个一定有序，另一个可能有序，可能无序。如此循环.
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0:
+            return -1
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:  # 如果中间值是目标，直接返回
+                return mid
+            if nums[mid] < nums[right]:  # 证明 mid 到 right 之间是升序的
+                if nums[mid] < target <= nums[right]:  # 如果 target 在这个升序区间内
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                # 即nums[mid]>nums[right]的情况，因为各元素不同，因为 nums 是升序序列旋转得到，所以旋转点左右依然有序
+                # nums[right] 肯定是某一段的最大值，nums[mid]>nums[right]代表旋转点在mid右侧，也就是说mid左侧现在是有序的
+                if nums[left] <= target < nums[mid]:  # 如果target在左侧有序区间
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        if nums[left] == target:
+            return left
+        else:
+            return -1
+
+          
