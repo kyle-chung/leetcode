83b64181ec5198c4c808d47305ade9c880434a81
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-17 12:40:53 +0800

    Update 0042. 接雨水.py

--- 0042. 接雨水.py
+++ 0042. 接雨水.py
@@ -11,3 +11,28 @@
 输入：height = [4,2,0,3,2,5]
 输出：9
 
+# dp 时间复杂度：O(n)
+创建两个长度为 n 的数组 leftMax 和 rightMax。对于 0 ≤ i < n, leftMax[i] 表示下标 i 及其左边的位置中，height 的最大高度
+rightMax[i] 表示下标 i 及其右边的位置中，height 的最大高度
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        
+        n = len(height)
+        leftMax = [height[0]] + [0] * (n - 1)
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], height[i])
+
+        rightMax = [0] * (n - 1) + [height[n - 1]]
+        for i in range(n - 2, -1, -1):
+            rightMax[i] = max(rightMax[i + 1], height[i])
+
+        ans = sum(min(leftMax[i], rightMax[i]) - height[i] for i in range(n))
+        return ans
+
+      
+      
+      
+      
