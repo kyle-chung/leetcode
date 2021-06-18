089cf388c1b4541adf9ab885f0408db0251cc225
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-18 13:53:00 +0800

    Update 0039. 组合总和.py

--- 0039. 组合总和.py
+++ 0039. 组合总和.py
@@ -37,16 +37,20 @@ candidates 中的数字可以无限制重复被选取。
 
 class Solution:
     def combinationSum(self, candidates, target):
-        ans = []
-        temp = []
-        def recursion(idx, res):
-            if idx >= len(candidates) or res >= target:
-                if res == target:
-                    ans.append(temp[:])
+        res = []
+        def combination(candidates,target,res_list):
+            if target < 0:
                 return
-            temp.append(candidates[idx])
-            recursion(idx, res + candidates[idx]) 
-            temp.pop()
-            recursion(idx + 1, res)
-        recursion(0, 0)
-        return ans 
+            if target == 0:
+                res.append(res_list)
+            for i,c in enumerate(candidates):
+                # 为了避免重复 (例如candiactes=[2,3,6,7],target=7，输出[[2,2,3],[3,2,2][7]])
+                # 传到的下一个candicate为candicates[i:]
+                combination(candidates[i:],target-c,res_list+[c])
+        
+        combination(candidates,target,[])
+        return res
+
+
+      
+      
