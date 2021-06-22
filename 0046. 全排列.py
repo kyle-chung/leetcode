37953b37aead707a59e61989992e8f8c61324598
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-22 16:38:02 +0800

    Update 0046. 全排列.py

--- 0046. 全排列.py
+++ 0046. 全排列.py
@@ -10,6 +10,26 @@
 输入：nums = [0,1]
 输出：[[0,1],[1,0]]
 
+# backtracking
+
+class Solution:
+    def permute(self, nums):
+        def backtrack(first = 0):
+            # 所有数都填完了
+            if first == n:  
+                res.append(nums[:])
+            for i in range(first, n):
+                # 动态维护数组
+                nums[first], nums[i] = nums[i], nums[first]
+                # 继续递归填下一个数
+                backtrack(first + 1)
+                # 撤销操作
+                nums[first], nums[i] = nums[i], nums[first]
+        
+        n = len(nums)
+        res = []
+        backtrack()
+        return res
 
 
 
