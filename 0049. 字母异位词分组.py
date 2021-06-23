41262d7324a5ed24f7ff35c0e33fb1dabce1a5f9
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-23 17:56:42 +0800

    Update 0049. 字母异位词分组.py

--- 0049. 字母异位词分组.py
+++ 0049. 字母异位词分组.py
@@ -10,3 +10,17 @@
   ["bat"]
 ]
 
+# 排序
+由于互为字母异位词的两个字符串包含的字母相同，因此对两个字符串分别进行排序之后得到的字符串一定是相同的
+故可以将排序之后的字符串作为哈希表的键。
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = collections.defaultdict(list)
+
+        for st in strs:
+            key = "".join(sorted(st))
+            mp[key].append(st) # 若mp中key values不包含key，则会自动创建
+        
+        return list(mp.values())
+
