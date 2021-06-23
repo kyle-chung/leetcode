3c4b58e2e8df948b558d7f50bb556787724f0d94
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-06-23 18:10:49 +0800

    Update 0049. 字母异位词分组.py

--- 0049. 字母异位词分组.py
+++ 0049. 字母异位词分组.py
@@ -24,3 +24,21 @@ class Solution:
         
         return list(mp.values())
 
+计数
+由于互为字母异位词的两个字符串包含的字母相同，因此两个字符串中的相同字母出现的次数一定是相同的
+故可以将每个字母出现的次数使用字符串表示，作为哈希表的键。
+
+由于字符串只包含小写字母，因此对于每个字符串，可以使用长度为 26 的数组记录每个字母出现的次数
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = collections.defaultdict(list)
+
+        for st in strs:
+            counts = [0] * 26
+            for ch in st:
+                counts[ord(ch) - ord("a")] += 1
+            # 需要将 list 转换成 tuple 才能进行哈希
+            mp[tuple(counts)].append(st)
+        
+        return list(mp.values())
