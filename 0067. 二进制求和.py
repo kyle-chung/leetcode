48cd6ca53b5fee2b8390eceffca3ef24b97c3164
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-07-01 16:21:20 +0800

    Update 0067. 二进制求和.py

--- 0067. 二进制求和.py
+++ 0067. 二进制求和.py
@@ -7,3 +7,33 @@
 输入: a = "11", b = "1"
 输出: "100"
 
+# 模拟
+先将 a 和 b 转化成十进制数，求和后再转化为二进制数
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = str(int(a)+int(b))
+        res = list(res)
+        
+        for i in range(-1,-len(res)-1,-1):
+            if int(res[i])//2 == 0:
+                continue
+            else:
+                res[i] = str(int(res[i])%2)
+                try:
+                    res[i-1] = str(int(res[i-1])+1)
+                except:
+                    res = ['1']+res
+                    return ''.join(res)
+
+        return ''.join(res)
+  
+# 位运算
+class Solution:
+    def addBinary(self, a, b) -> str:
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            answer = x ^ y
+            carry = (x & y) << 1
+            x, y = answer, carry
+        return bin(x)[2:]
+
