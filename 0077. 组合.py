209eb38c783d81a6fe7ec4bb9c62502fd2dfd045
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-07-06 13:43:04 +0800

    Update 0077. 组合.py

--- 0077. 组合.py
+++ 0077. 组合.py
@@ -12,3 +12,34 @@
   [1,3],
   [1,4],
 ]
+
+# backtrack求过易懂框架版
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result, track = [], []
+        self.backtrack(n, k, 1, track, result)
+        return result
+
+    def backtrack(self, n, k, start, track, result):
+        if len(track) == k:
+            result.append(track[:])
+        for i in range(start, n+1):
+            track.append(i)
+            self.backtrack(n, k, i+1, track, result)
+            track.pop()
+            
+# backtrack剪枝版            
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result, track = [], []
+        self.backtrack(n, k, 1, track, result)
+        return result
+
+    def backtrack(self, n, k, start, track, result):
+        if k == 0:
+            result.append(track[:])
+            return
+        for i in range(start, n-k+2):
+            track.append(i)
+            self.backtrack(n, k-1, i+1, track, result)
+            track.pop()
