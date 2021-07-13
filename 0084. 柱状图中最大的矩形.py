56344429abec29d43b0378947f1bb0db05b0fcd0
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-07-13 23:52:07 +0800

    Update 0084. 柱状图中最大的矩形.py

--- 0084. 柱状图中最大的矩形.py
+++ 0084. 柱状图中最大的矩形.py
@@ -68,6 +68,27 @@ class Solution:
 
 栈：[2(3), 3(7)]
 
+# 单调栈 + 常数优化 时间复杂度：O(N)
+在方法一中，我们在对位置 i 进行入栈操作时，确定了它的左边界
+从直觉上来说，与之对应的我们在对位置 i 进行出栈操作时可以确定它的右边界
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        left, right = [0] * n, [n] * n
+
+        mono_stack = list()
+        for i in range(n):
+            while mono_stack and heights[mono_stack[-1]] >= heights[i]:
+                right[mono_stack[-1]] = i
+                mono_stack.pop()
+            left[i] = mono_stack[-1] if mono_stack else -1
+            mono_stack.append(i)
+        
+        ans = max((right[i] - left[i] - 1) * heights[i] for i in range(n)) if n > 0 else 0
+        return ans
+
+
 
       
       
