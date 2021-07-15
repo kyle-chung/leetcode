9db570e9cb7f085995c041f2f7db27f4064ab7ca
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-07-15 14:31:26 +0800

    Update 0091. 解码方法.py

--- 0091. 解码方法.py
+++ 0091. 解码方法.py
@@ -16,3 +16,20 @@
 输入：s = "12"
 输出：2
 解释：它可以解码为 "AB"（1 2）或者 "L"（12）
+
+# dp 时间复杂度：O(n)
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        # a = f[i-2], b = f[i-1], c = f[i]
+        a, b, c = 0, 1, 0
+        for i in range(1, n + 1):
+            c = 0
+            if s[i - 1] != '0':
+                c += b
+            if i > 1 and s[i - 2] != '0' and int(s[i-2:i]) <= 26:
+                c += a
+            a, b = b, c
+        return c
+
+
