b5b80f172adb64811948adeac45c85e4aff803fd
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-07-18 10:55:55 +0800

    Update 0118. 杨辉三角.py

--- 0118. 杨辉三角.py
+++ 0118. 杨辉三角.py
@@ -13,3 +13,20 @@
   [1,3,3,1],
  [1,4,6,4,1]
 ]
+
+# 一行一行地计算杨辉三角 时间复杂度：O(numRows^2）
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ret = list()
+        for i in range(numRows):
+            row = list()
+            for j in range(0, i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(ret[i - 1][j] + ret[i - 1][j - 1])
+            ret.append(row)
+        return ret
+
+
+
