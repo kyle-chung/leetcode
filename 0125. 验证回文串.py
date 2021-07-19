abb3746f84400e57db9922ea81b82902cf9a66a7
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-07-19 15:08:25 +0800

    Update and rename 0125. 验证回文串 to 0125. 验证回文串.py

---
+++ 0125. 验证回文串.py
@@ -0,0 +1,49 @@
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+说明：本题中，我们将空字符串定义为有效的回文串。
+
+示例 1:
+
+输入: "A man, a plan, a canal: Panama"
+输出: true
+解释："amanaplanacanalpanama" 是回文串
+
+# 双指针 时间复杂度：O(|s|)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sgood = "".join(ch.lower() for ch in s if ch.isalnum())
+        n = len(sgood)
+        left, right = 0, n - 1
+        
+        while left < right:
+            if sgood[left] != sgood[right]:
+                return False
+            left, right = left + 1, right - 1
+        return True
+
+# 空间优化
+每次将指针移到下一个字母字符或数字字符，再判断这两个指针指向的字符是否相同
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n = len(s)
+        left, right = 0, n - 1
+        
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if left < right:
+                if s[left].lower() != s[right].lower():
+                    return False
+                left, right = left + 1, right - 1
+
+        return True
+
+
+
+      
+      
+      
+      
