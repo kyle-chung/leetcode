45f0a34e43006ba0606172d1649b3b0c76357883
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-07-22 16:04:26 +0800

    Update 0130. 被围绕的区域.py

--- 0130. 被围绕的区域.py
+++ 0130. 被围绕的区域.py
@@ -11,4 +11,75 @@
 
 
 
+首先对边界上每一个'O'做深度优先搜索，将与其相连的所有'O'改为'A'
+然后遍历矩阵，将矩阵中所有'O'改为'X',将矩阵中所有'A'变为'O' 
+
+# dfs 时间复杂度：O(n×m)
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board:
+            return
+        
+        n, m = len(board), len(board[0])
+
+        def dfs(x, y):
+            if not 0 <= x < n or not 0 <= y < m or board[x][y] != 'O':
+                return
+            
+            board[x][y] = "A"
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
+        
+        for i in range(n):
+            dfs(i, 0)
+            dfs(i, m - 1)
+        
+        for i in range(m - 1):
+            dfs(0, i)
+            dfs(n - 1, i)
+        
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == "A":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
+
+# bfs 时间复杂度：O(n×m)
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board:
+            return
+        
+        n, m = len(board), len(board[0])
+        que = collections.deque()
+        for i in range(n):
+            if board[i][0] == "O":
+                que.append((i, 0))
+            if board[i][m - 1] == "O":
+                que.append((i, m - 1))
+        for i in range(m - 1):
+            if board[0][i] == "O":
+                que.append((0, i))
+            if board[n - 1][i] == "O":
+                que.append((n - 1, i))
+        
+        while que:
+            x, y = que.popleft()
+            board[x][y] = "A"
+            for mx, my in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+                if 0 <= mx < n and 0 <= my < m and board[mx][my] == "O":
+                    que.append((mx, my))
+        
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == "A":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
+
+                    
+
 
