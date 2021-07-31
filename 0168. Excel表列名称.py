5dedb866d70f1d01375b8e56587f4e02e8c3158c
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-07-31 22:24:33 +0800

    Create 0168. Excel表列名称.py

---
+++ 0168. Excel表列名称.py
@@ -0,0 +1,24 @@
+给你一个整数 columnNumber ，返回它在 Excel 表中相对应的列名称。
+
+例如：
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+示例 1：
+
+输入：columnNumber = 1
+输出："A"
+示例 2：
+
+输入：columnNumber = 28
+输出："AB"
+
+
+
