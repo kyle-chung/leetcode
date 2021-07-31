89b87d94d7f6ba9ded56ce1de22f4294b6e40e41
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-07-31 23:41:23 +0800

    Update 0168. Excel表列名称.py

--- 0168. Excel表列名称.py
+++ 0168. Excel表列名称.py
@@ -20,5 +20,21 @@ AB -> 28
 输入：columnNumber = 28
 输出："AB"
 
+# O(log 26 * columnNumber)
+这是一道从 1 开始的的 26 进制转换题。
+
+对于一般性的进制转换题目，只需要不断地对 columnNumber 进行 % 运算取得最后一位
+然后对 columnNumber 进行 // 运算，将已经取得的位数去掉，直到 columnNumber 为 0 即可。
+
+但本题需要我们将从 1 开始，因此在执行「进制转换」操作前，我们需要先对 columnNumber 执行减一操作，从而实现整体偏移
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        ans = list()
+        while columnNumber > 0:
+            columnNumber -= 1
+            ans.append(chr(columnNumber % 26 + ord("A")))
+            columnNumber //= 26
+        return "".join(ans[::-1])
 
 
