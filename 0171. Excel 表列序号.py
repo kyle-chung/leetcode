5cf2ca891a80ef829d370a7238a6866aa69e394f
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-08-01 13:20:24 +0800

    Update 0171. Excel 表列序号.py

--- 0171. Excel 表列序号.py
+++ 0171. Excel 表列序号.py
@@ -20,3 +20,15 @@
 输入: columnTitle = "AB"
 输出: 28
 
+# 进制转换 O(n)
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        number, multiple = 0, 1
+        for i in range(len(columnTitle) - 1, -1, -1):
+            k = ord(columnTitle[i]) - ord("A") + 1
+            number += k * multiple
+            multiple *= 26
+        return number
+
+
+
