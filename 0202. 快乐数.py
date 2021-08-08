11dbf1d78ad9016a48a91f58dc25dd5075176fe8
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-08-08 16:45:02 +0800

    Update 0202. 快乐数.py

--- 0202. 快乐数.py
+++ 0202. 快乐数.py
@@ -18,3 +18,27 @@
 6**2 + 8**2 = 100
 1**2 + 0**2 + 0**2 = 1
 
+# hashmap
+首先按照题目的要求做数位分离，求平方和。
+
+每次生成下一个数字时，我们都会检查它是否已经在哈希集合中。
+
+如果它不在哈希集合中，我们应该添加它。
+如果它在哈希集合中，这意味着我们处于一个循环中，因此应该返回 false
+
+def isHappy(self, n: int) -> bool:
+    def get_next(n):
+        total_sum = 0
+        while n > 0:
+            n, digit = divmod(n, 10)
+            total_sum += digit ** 2
+        return total_sum
+
+    seen = set()
+    while n != 1 and n not in seen:
+        seen.add(n)
+        n = get_next(n)
+
+    return n == 1
+
+
