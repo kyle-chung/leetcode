b699432701f4d4192005d9ff5e60da584ee21326
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-08-08 17:03:40 +0800

    Update 0202. 快乐数.py

--- 0202. 快乐数.py
+++ 0202. 快乐数.py
@@ -42,6 +42,34 @@ def isHappy(self, n: int) -> bool:
     return n == 1
 python divmod() 函数把除数和余数运算结果结合起来，返回一个包含商和余数的元组(a // b, a % b)。
 
+# 快慢指针法 时间复杂度：O(logn)
+通过反复调用 getNext(n) 得到的链是一个隐式的链表
+隐式意味着我们没有实际的链表节点和指针，但数据仍然形成链表结构
+起始数字是链表的头 “节点”，链中的所有其他数字都是节点。next 指针是通过调用 getNext(n) 函数获得。
+
+意识到我们实际有个链表，那么这个问题就可以转换为检测一个链表是否有环。因此我们在这里可以使用弗洛伊德循环查找算法
+
+我们不是只跟踪链表中的一个值，而是跟踪两个值，称为快跑者和慢跑者
+在算法的每一步中，慢速在链表中前进 1 个节点，快跑者前进 2 个节点（对 getNext(n) 函数的嵌套调用）。
+
+如果 n 是一个快乐数，即没有循环，那么快跑者最终会比慢跑者先到达数字 1。
+
+如果 n 不是一个快乐的数字，那么最终快跑者和慢跑者将在同一个数字上相遇
+
+def isHappy(self, n: int) -> bool:  
+    def get_next(number):
+        total_sum = 0
+        while number > 0:
+            number, digit = divmod(number, 10)
+            total_sum += digit ** 2
+        return total_sum
+
+    slow_runner = n
+    fast_runner = get_next(n)
+    while fast_runner != 1 and slow_runner != fast_runner:
+        slow_runner = get_next(slow_runner)
+        fast_runner = get_next(get_next(fast_runner))
+    return fast_runner == 1
 
 
 
