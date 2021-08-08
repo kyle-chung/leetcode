fc99aacfad11737f0487dda63a5f16cc1da4be83
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-08-08 15:44:17 +0800

    Update 0201. 数字范围按位与.py

--- 0201. 数字范围按位与.py
+++ 0201. 数字范围按位与.py
@@ -9,3 +9,41 @@
 输入：left = 0, right = 0
 输出：0
 
+观察按位与运算的性质。对于一系列的位，例如 [1, 1, 0, 1, 1]，只要有一个零值的位，那么这一系列位的按位与运算结果都将为零
+
+因此，最终我们可以将问题重新表述为：给定两个整数，我们要找到它们对应的二进制字符串的公共前缀
+
+# 位移 时间复杂度：O(logn)
+class Solution:
+    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+        shift = 0   
+        # 找到公共前缀
+        while m < n:
+            m = m >> 1
+            n = n >> 1
+            shift += 1
+        return m << shift
+      
+# Brian Kernighan 算法
+Brian Kernighan 算法的关键在于我们每次对 number 和 number−1 之间进行按位与运算后，number 中最右边的 1 会被抹去变成 0
+我们可以对数字 n 迭代地应用上述技巧，清除最右边的 1，直到它小于或等于 m，此时非公共前缀部分的 1 均被消去。因此最后我们返回 n 即可
+
+class Solution:
+    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+        while m < n:
+            # 抹去最右边的 1
+            n = n & (n - 1)
+        return n
+
+
+
+
+
+
+
+
+
+
+
+
+
