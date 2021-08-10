6dfa50a1765bb730afa8360cd9978961174506a5
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-08-10 19:23:10 +0800

    Update 0204. 计数质数.py

--- 0204. 计数质数.py
+++ 0204. 计数质数.py
@@ -6,16 +6,21 @@
 输出：4
 解释：小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
 
-# 枚举
-def countPrimes(n):
-    is_prime = [True]*(n)
-    ans = 0
-    for num in range(2,n): 
-        if is_prime[num]:
-            ans+=1
-            # 右边界:因为数字最大是n-1 所以只需要到(n-1)//num 右边是开区间 所以+1
-            for k in range(1,(n-1)//num+1):
-                is_prime[num*k]=False
-    return ans
+# 埃氏筛
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        # 定义数组标记是否是质数
+        is_prime = [1] * n
+        
+        count = 0
+        for i in range(2, n):
+            # 将质数的倍数标记为合数
+            if is_prime[i]:
+                count += 1
+                # 从 i*i 开始标记
+                for j in range(i*i, n, i):
+                    is_prime[j] = 0
+
+        return count
 
 
