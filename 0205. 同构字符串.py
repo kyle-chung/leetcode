660778e0e6d2465af347652878813a55a142b18e
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-08-10 15:11:50 +0800

    Update 0205. 同构字符串.py

--- 0205. 同构字符串.py
+++ 0205. 同构字符串.py
@@ -29,11 +29,11 @@ class Solution:
 # haspmap
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        dic1=dict()
-        dic2=dict()
+        dic1 = dict()
+        dic2 = dict()
         for i in range(len(s)):
-            if (s[i] in dic1 and dic1[s[i]]!=t[i]) or (t[i] in dic2 and dic2[t[i]]!=s[i]):
+            if (s[i] in dic1 and dic1[s[i]] != t[i]) or (t[i] in dic2 and dic2[t[i]] != s[i]):
                 return False
-            dic1[s[i]]=t[i]
-            dic2[t[i]]=s[i]
+            dic1[s[i]] = t[i]
+            dic2[t[i]] = s[i]
         return True
