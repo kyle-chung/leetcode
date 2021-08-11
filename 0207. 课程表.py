a723b6130b5f54548e42c5abdb1ee1614b45c275
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-08-12 00:10:22 +0800

    Update 0207. 课程表.py

--- 0207. 课程表.py
+++ 0207. 课程表.py
@@ -67,6 +67,48 @@ class Solution:
                 if not indegrees[cur]: queue.append(cur)
         return not numCourses
 
+# dfs
+
+算法流程：
+
+借助一个标志列表 flags，用于判断每个节点 i （课程）的状态：
+未被 DFS 访问：i == 0；
+已被其他节点启动的 DFS 访问：i == -1；
+已被当前节点启动的 DFS 访问：i == 1
+
+对 numCourses 个节点依次执行 DFS，判断每个节点起步 DFS 是否存在环，若存在环直接返回 False
+DFS 流程：
+
+终止条件：
+当 flag[i] == -1，说明当前访问节点已被其他节点启动的 DFS 访问，无需再重复搜索，直接返回 True
+当 flag[i] == 1，说明在本轮 DFS 搜索中节点 i 被第 2 次访问，即 课程安排图有环 ，直接返回 False
+
+将当前访问节点 i 对应 flag[i] 置 11，即标记其被本轮 DFS 访问过
+
+递归访问当前节点 i 的所有邻接节点 j，当发现环直接返回 False
+
+当前节点所有邻接节点已被遍历，并没有发现环，则将当前节点 flag 置为 -1 并返回 True
+
+若整个图 DFS 结束并未发现环，返回 True
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        def dfs(i, adjacency, flags):
+            if flags[i] == -1: return True
+            if flags[i] == 1: return False
+            flags[i] = 1
+            for j in adjacency[i]:
+                if not dfs(j, adjacency, flags): return False
+            flags[i] = -1
+            return True
+
+        adjacency = [[] for _ in range(numCourses)]
+        flags = [0 for _ in range(numCourses)]
+        for cur, pre in prerequisites:
+            adjacency[pre].append(cur)
+        for i in range(numCourses):
+            if not dfs(i, adjacency, flags): return False
+        return True
 
 
 
