3ca5d99a1da141ca5c9befa7d6734655a6d5e0c4
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-08-12 13:32:41 +0800

    Update 0209. 长度最小的子数组.py

--- 0209. 长度最小的子数组.py
+++ 0209. 长度最小的子数组.py
@@ -13,6 +13,34 @@
 输入：target = 4, nums = [1,4,4]
 输出：1
 
+# 前缀和 + 二分查找
+
+为了使用二分查找，需要额外创建一个数组 sums 用于存储数组 nums 的前缀和
+其中 sums[i] 表示从 nums[0] 到 nums[i−1] 的元素和
+得到前缀和之后，对于每个开始下标 i，可通过二分查找得到大于或等于 i 的最小下标 bound，使得 sums[bound]−sums[i−1] ≥ target
+并更新子数组的最小长度（此时子数组的长度是 bound−(i−1)）
+
+因为这道题保证了数组中每个元素都为正，所以前缀和一定是递增的，这一点保证了二分的正确性
+
+class Solution:
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        ans = n + 1
+        sums = [0]
+        for i in range(n):
+            sums.append(sums[-1] + nums[i])
+        
+        for i in range(1, n + 1):
+            target = s + sums[i - 1]
+            bound = bisect.bisect_left(sums, target)
+            if bound != len(sums):
+                ans = min(ans, bound - (i - 1))
+        
+        return 0 if ans == n + 1 else ans
+
 
 
 
