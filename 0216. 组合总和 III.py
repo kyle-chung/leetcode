f36ca65c3ff3b49efc44b30d01d7e7b0e51906b6
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-08-19 00:03:27 +0800

    Update 0216. 组合总和 III.py

--- 0216. 组合总和 III.py
+++ 0216. 组合总和 III.py
@@ -15,5 +15,26 @@
 输入: k = 3, n = 9
 输出: [[1,2,6], [1,3,5], [2,3,4]]
   
+# backtrack 如何剪枝是关键
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        if n < 6: return []
+        res = []
+        def backtrack(num, path, k, n):
+            if k == 0 and n == 0:
+                res.append(path[:])
+            if n < 0 or k < 0:
+                return 
+            for cn in range(num, 10):
+                if cn > n:
+                    break
+                path.append(cn)
+                backtrack(cn+1, path, k-1, n-cn)
+                path.pop()
+            return 
+
+        backtrack(1, [], k, n)
+        return res
+  
   
   
