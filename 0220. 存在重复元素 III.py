a1f9bd3b09788b63f7d3310371f659a3344e6b27
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-09-14 16:28:03 +0800

    Update 0220. 存在重复元素 III.py

--- 0220. 存在重复元素 III.py
+++ 0220. 存在重复元素 III.py
@@ -31,6 +31,34 @@ class Solution:
       
 时间复杂度：TreeSet 基于红黑树，查找和插入都是 O(logk) 复杂度。整体复杂度为 O(nlogk)
 
+# 桶排序 O(n)
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        def getIdx(u):
+            return ((u + 1) // size) - 1 if u < 0 else u // size
+        
+        map = {}
+        size = t + 1
+        for i,u in enumerate(nums):
+            idx = getIdx(u)
+            # 目标桶已存在（桶不为空），说明前面已有 [u - t, u + t] 范围的数字
+            if idx in map:
+                return True
+            # 检查相邻的桶
+            l, r = idx - 1, idx + 1
+            if l in map and abs(u - map[l]) <= t:
+                return True
+            if r in map and abs(u - map[r]) <= t:
+                return True
+            # 建立目标桶
+            map[idx] = u
+            # 维护个数为k
+            if i >= k:
+                map.pop(getIdx(nums[i-k]))
+
+        return False
+
+
 
       
       
