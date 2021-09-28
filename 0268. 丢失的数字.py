e49883c0d5b378de9b1fbb3dcffb8f4a28123f63
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-09-28 13:39:08 +0800

    Update 0268. 丢失的数字.py

--- 0268. 丢失的数字.py
+++ 0268. 丢失的数字.py
@@ -7,3 +7,21 @@ nums 中的所有数字都 独一无二
 输出：2
 解释：n = 3，因为有 3 个数字，所以所有的数字都在范围 [0,3] 内。2 是丢失的数字，因为它没有出现在 nums 中。
 
+# 位运算
+由于异或运算（XOR）满足结合律，并且对一个数进行两次完全相同的异或运算会得到原来的数，因此我们可以通过异或运算找到缺失的数字
+
+class Solution:
+    def missingNumber(self, nums):
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        return missing
+
+# math
+class Solution:
+    def missingNumber(self, nums):
+        expected_sum = len(nums)*(len(nums)+1)//2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
+
+
