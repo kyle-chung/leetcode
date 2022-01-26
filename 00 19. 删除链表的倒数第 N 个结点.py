825e857aab5e5d5ec32fa1846dfb4a359e4b672a
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2022-01-26 22:22:43 +0800

    Create 00 19. 删除链表的倒数第 N 个结点.py

---
+++ 00 19. 删除链表的倒数第 N 个结点.py
@@ -0,0 +1,34 @@
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+
+示例 1：
+
+
+输入：head = [1,2,3,4,5], n = 2
+输出：[1,2,3,5]
+
+示例 2：
+
+输入：head = [1], n = 1
+输出：[]
+
+示例 3：
+
+输入：head = [1,2], n = 1
+输出：[1]
+
+# 双指针 O(L) O(1)
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0, head)
+        first = head
+        second = dummy
+        for i in range(n):
+            first = first.next
+
+        while first:
+            first = first.next
+            second = second.next
+        
+        second.next = second.next.next
+        return dummy.next
+
