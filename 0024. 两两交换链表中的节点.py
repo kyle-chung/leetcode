d4a0fd701ffea42748e7b730252cf03f864182b1
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2022-01-30 00:28:50 +0800

    Create 0024. 两两交换链表中的节点.py

---
+++ 0024. 两两交换链表中的节点.py
@@ -0,0 +1,18 @@
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        dummyHead = ListNode(0)
+        dummyHead.next = head
+        temp = dummyHead
+        while temp.next and temp.next.next:
+            node1 = temp.next
+            node2 = temp.next.next
+            temp.next = node2
+            node1.next = node2.next
+            node2.next = node1
+            temp = node1
+        return dummyHead.next
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/liang-liang-jiao-huan-lian-biao-zhong-de-jie-di-91/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
