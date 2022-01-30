8c6b7e61841ff762ddd0a58da8444d0442f93de9
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2022-01-30 11:46:42 +0800

    Update 0024. 两两交换链表中的节点.py

--- 0024. 两两交换链表中的节点.py
+++ 0024. 两两交换链表中的节点.py
@@ -1,3 +1,14 @@
+# recur O(n) O(n)
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        newHead = head.next
+        head.next = self.swapPairs(newHead.next)
+        newHead.next = head
+        return newHead
+
+# 迭代 O(n) O(1)
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
         dummyHead = ListNode(0)
