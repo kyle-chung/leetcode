e7c2efeaef8df5864f5e99149339813b2e1d56c2
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2022-02-06 23:46:01 +0800

    Create 23. 合并K个升序链表.java

---
+++ 23. 合并K个升序链表.java
@@ -0,0 +1,87 @@
+给你一个链表数组，每个链表都已经按升序排列。
+
+请你将所有链表合并到一个升序链表中，返回合并后的链表。
+
+示例 1：
+
+输入：lists = [[1,4,5],[1,3,4],[2,6]]
+输出：[1,1,2,3,4,4,5,6]
+解释：链表数组如下：
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+将它们合并到一个有序链表中得到。
+1->1->2->3->4->4->5->6
+  
+示例 2：
+
+输入：lists = []
+输出：[]
+
+示例 3：
+
+输入：lists = [[]]
+输出：[]
+
+前置知识：合并两个有序链表
+
+假设链表 a 和 b 的长度都是 n，如何在 O(n) 的时间代价以及 O(1) 的空间代价完成合并？ 
+为了达到空间代价是 O(1)，宗旨是「原地调整链表元素的 next 指针完成合并」
+
+public ListNode mergeTwoLists(ListNode a, ListNode b) {
+    if (a == null || b == null) {
+        return a != null ? a : b;
+    }
+    ListNode head = new ListNode(0);
+    ListNode tail = head, aPtr = a, bPtr = b;
+    while (aPtr != null && bPtr != null) {
+        if (aPtr.val < bPtr.val) {
+            tail.next = aPtr;
+            aPtr = aPtr.next;
+        } else {
+            tail.next = bPtr;
+            bPtr = bPtr.next;
+        }
+        tail = tail.next;
+    }
+    tail.next = (aPtr != null ? aPtr : bPtr);
+    return head.next;
+}
+
+方法一：顺序合并
+用一个变量 ans 来维护以及合并的链表，第 i 次循环把第 i 个链表和 ans 合并，答案保存到 ans 中
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode ans = null;
+        for (int i = 0; i < lists.length; ++i) {
+            ans = mergeTwoLists(ans, lists[i]);
+        }
+        return ans;
+    }
+
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a != null ? a : b;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr != null ? aPtr : bPtr);
+        return head.next;
+    }
+}
+
+
+
