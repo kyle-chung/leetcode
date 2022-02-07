08977d8481ea66e9c7bff699aac507433800f7e3
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2022-02-07 12:21:10 +0800

    Update 0023. 合并K个升序链表.java

--- 0023. 合并K个升序链表.java
+++ 0023. 合并K个升序链表.java
@@ -50,7 +50,7 @@ public ListNode mergeTwoLists(ListNode a, ListNode b) {
     return head.next;
 }
 
-方法一：顺序合并 O(k^2*n) O(1)
+方法一：顺序合并 O(k^2*n) O(1) 假设每个链表的最长长度是 n
 用一个变量 ans 来维护以及合并的链表，第 i 次循环把第 i 个链表和 ans 合并，答案保存到 ans 中
 
 class Solution {
@@ -83,5 +83,47 @@ class Solution {
     }
 }
 
+方法二：分治合并 O(kn*logk) O(logk)
 
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    public ListNode merge(ListNode[] lists, int l, int r) {
+        if (l == r) {
+            return lists[l];
+        }
+        if (l > r) {
+            return null;
+        }
+        int mid = (l + r) >> 1;
+        return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+    }
+
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a != null ? a : b;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr != null ? aPtr : bPtr);
+        return head.next;
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/merge-k-sorted-lists/solution/he-bing-kge-pai-xu-lian-biao-by-leetcode-solutio-2/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
